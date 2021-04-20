d13f5a748f95a01ff6364055c8d635204e78c4a2
masutaniR <popohu87@gmail.com>
2021-04-20 12:02:43 +0900

    [Finish] java-4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+    	String name = "mstn";
+    	System.out.println(name);
+
+    	name = "パスポート";
+    	System.out.println(name);
+
+    	final int LUCKY_NUM = 82;
+    	System.out.println(LUCKY_NUM);
+    }
+}
