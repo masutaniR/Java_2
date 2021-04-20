70eb754f7a659e0c90a46da550b8161430852436
masutaniR <popohu87@gmail.com>
2021-04-20 12:35:17 +0900

    [Finish] java-5

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,22 @@
+public class Chapter05 {
+    public static void main(String[] args) {
+    	long long_num = 999999988888L;
+    	double double_num = 333551.44441;
+    	boolean boo = true;
+
+    	System.out.println(long_num);
+    	System.out.println(double_num);
+    	System.out.println(boo);
+
+    	Float float_num = 33.5F;
+    	Integer int1 = 9000;
+    	Character char1 = 'a';
+
+    	System.out.println(float_num);
+    	System.out.println(int1);
+    	System.out.println(char1);
+
+    	int[] array1 = { 8, 2, 9, 0, 4 };
+    	System.out.println(array1[3]);
+    }
+}
